293042dd2ca9ccd26fbac6ec3198128a64c0f89f
Eddie115-x <108811223+Eddie115-x@users.noreply.github.com>
2025-06-27 17:47:49 +1200

    resolve product list error refered to hom einstead not import product list

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,13 @@ import { createBrowserRouter } from 'react-router-dom';
 import RootLayout from './layouts/MainLayout';
 
 // Import pages
-import Admin from './pages/AdminDashboard';
+import AdminDashboard from './pages/AdminDashboard';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Orders from './pages/Orders';
 import ProductDetail from './pages/ProductDetail';
-import ProductList from './pages/ProductList';
 import Register from './pages/Register';
 
 const router = createBrowserRouter([
@@ -20,14 +19,14 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { index: true, element: <Home /> },
-      { path: 'products', element: <ProductList /> },
+      { path: 'products', element: <Home /> },
       { path: 'product/:id', element: <ProductDetail /> },
       { path: 'cart', element: <Cart /> },
       { path: 'checkout', element: <Checkout /> },
       { path: 'login', element: <Login /> },
       { path: 'register', element: <Register /> },
       { path: 'orders', element: <Orders /> },
-      { path: 'admin', element: <Admin /> }
+      { path: 'admin', element: <AdminDashboard /> }
     ]
   }
 ]);
