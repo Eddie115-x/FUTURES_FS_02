260517c1e4214def86cd7344bb245b2951d3bf11
Eddie115-x <108811223+Eddie115-x@users.noreply.github.com>
2025-06-27 17:54:38 +1200

    updated router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { CartProvider } from './context/CartContext';
-import Routes from './routes.js';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes.js';
 
 function App() {
   return (
     <CartProvider>
-      <Routes />
+      <RouterProvider router={router} />
     </CartProvider>
   );
 }
